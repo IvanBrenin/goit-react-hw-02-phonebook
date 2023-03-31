4086d3d2bcbe6eb227a0b9ef008e8f8852dc3249
Ivan Brenin <rzhevskiy.elite@gmail.com>
2023-03-31 13:10:54 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -11,7 +11,7 @@ const Filter = ({ value, onChange }) => {
 };
 
 Filter.propTypes = {
-  value: PropTypes.string.isRequired,
+  value: PropTypes.string,
   onChange: PropTypes.func.isRequired,
 };
 
